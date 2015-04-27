60ac5cc33feded49956a34bba675ea6a5d011a80
Elliot Humphrey <evhumphrey@users.noreply.github.com>
2015-04-27 16:24:36 -0700

    added Hien's d3 code that modifies existing d3 visualization; added navbar to this page

--- public/js/visualization.js
+++ public/js/visualization.js
@@ -1,6 +1,7 @@
 var margin = {top: 20, right: 20, bottom: 100, left: 40};
-var width = 960 - margin.left - margin.right;
-var height = 500 - margin.top - margin.bottom;
+    width = 960 - margin.left - margin.right;
+    height = 500 - margin.top - margin.bottom;
+// var formatPercent = d3.format(".0%");
 
 //define scale of x to be from 0 to width of SVG, with .1 padding in between
 var scaleX = d3.scale.ordinal()
@@ -18,6 +19,14 @@ var xAxis = d3.svg.axis()
 var yAxis = d3.svg.axis()
   .scale(scaleY)
   .orient("left");
+  // .tickFormat(formatPercent);
+
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(function(d) {
+    return "<strong>#Photos:</strong> <span style='color:red'>" + d.counts.media + "</span>";
+});
 
 //create svg
 var svg = d3.select("body").append("svg")
@@ -26,8 +35,10 @@ var svg = d3.select("body").append("svg")
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+svg.call(tip);
 //get json object which contains media counts
 d3.json('/igMediaCounts', function(error, data) {
+
   //set domain of x to be all the usernames contained in the data
   scaleX.domain(data.users.map(function(d) { return d.username; }));
   //set domain of y to be from 0 to the maximum media count returned
@@ -38,13 +49,13 @@ d3.json('/igMediaCounts', function(error, data) {
     .attr("class", "x axis")
     .attr("transform", "translate(0," + height + ")") //move x-axis to the bottom
     .call(xAxis)
-    .selectAll("text")  
-    .style("text-anchor", "end")
-    .attr("dx", "-.8em")
-    .attr("dy", ".15em")
-    .attr("transform", function(d) {
-      return "rotate(-65)" 
-    });
+    // .selectAll("text")  
+    // .style("text-anchor", "end")
+    // .attr("dx", "-.8em")
+    // // .attr("dy", ".15em")
+    // .attr("transform", function(d) {
+    //   return "rotate(-20)" 
+    // });
 
   //set up y axis
   svg.append("g")
@@ -65,5 +76,41 @@ d3.json('/igMediaCounts', function(error, data) {
     .attr("x", function(d) { return scaleX(d.username); })
     .attr("width", scaleX.rangeBand())
     .attr("y", function(d) { return scaleY(d.counts.media); })
-    .attr("height", function(d) { return height - scaleY(d.counts.media); });
+    .attr("height", function(d) { return height - scaleY(d.counts.media); })
+    .on('mouseover', tip.show)
+    .on('mouseout', tip.hide);
+
+
+
+  d3.select("input").on("change", change);
+  var sortTimeout = setTimeout(function() {
+    d3.select("input").property("checked", true).each(change);
+  }, 2000);
+
+  function change() {
+    clearTimeout(sortTimeout);
+
+    // Copy-on-write since tweens are evaluated after a delay.
+    var x0 = scaleX.domain(data.users.sort(this.checked
+        ? function(a, b) { return b.counts.media - a.counts.media; }
+        : function(a, b) { return d3.ascending(a.username, b.username); })
+        .map(function(d) { return d.username; }))
+        .copy();
+
+    svg.selectAll(".bar")
+        .sort(function(a, b) { return x0(a.username) - x0(b.username); });
+
+    var transition = svg.transition().duration(750),
+        delay = function(d, i) { return i * 50; };
+
+    transition.selectAll(".bar")
+        .delay(delay)
+        .attr("x", function(d) { return x0(d.username); });
+
+    transition.select(".x.axis")
+        .call(xAxis)
+      .selectAll("g")
+        .delay(delay);
+  }
+
 });
